dec8584777ca453396c65f9aa0d4257c86ca6c43
Christian Di Lorenzo <rcdilorenzo@me.com>
2015-07-31 15:59:46 -0400

    Add padding for transaction name

--- src/models/transaction.erl
+++ src/models/transaction.erl
@@ -5,8 +5,8 @@
 -import(cli, [pad/1, pad/2, print_values/1, print_values/2]).
 
 print_header() ->
-    print_values([pad("ID", 5), pad("Name"), pad("Purchased"), pad("Vendor")], header).
+    print_values([pad("ID", 5), pad("Name", 50), pad("Purchased"), pad("Vendor")], header).
 
 print(#{id := ID, name := Name, purchased := Purchased, vendor := Vendor}) ->
-    print_values([pad(ID, 5), pad(Name), pad(Purchased), pad(Vendor)]).
+    print_values([pad(ID, 5), pad(Name, 50), pad(Purchased), pad(Vendor)]).
 
